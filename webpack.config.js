bb4bd97008e95fd41929a2d790f775b6085e8065
Johnny Quesada <johnny-qc@hotmail.com>
2023-04-04 20:25:51 +0200

    adding bundler

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    bundle: './src/index.ts',
+  },
+  externals: {
+    '@angular/core': '@angular/core',
+  },
+  output: {
+    path: path.resolve(__dirname, 'lib'),
+    filename: ({ chunk: { name } }) => {
+      if (name.includes('worker')) {
+        return `${name}.worker.js`;
+      }
+
+      return `${name}.js`;
+    },
+    libraryTarget: 'umd',
+    library: 'angular-changes-manager',
+    globalObject: 'this',
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+    alias: {
+      'angular-changes-manager': path.resolve(
+        __dirname,
+        'node_modules/angular-changes-manager/package.json'
+      ),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-typescript'],
+              plugins: [
+                '@babel/plugin-transform-modules-commonjs',
+                '@babel/plugin-proposal-class-properties',
+                '@babel/plugin-proposal-export-namespace-from',
+              ],
+            },
+          },
+          {
+            loader: 'ts-loader',
+          },
+        ],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+};
